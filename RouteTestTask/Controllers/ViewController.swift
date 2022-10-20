c280e95b6be6bc83722dc0556702037e13297dd5
Scooterminsk <kickscooterby@gmail.com>
2022-10-20 17:25:59 +0300

    addAddressButton was created and added

--- RouteTestTask/Controllers/ViewController.swift
+++ RouteTestTask/Controllers/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
         return mapView
     }()
     
+    lazy var addAddressButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "AddAddress"), for: .normal)
+        button.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,7 +34,11 @@ class ViewController: UIViewController {
 
     private func setupViews() {
         view.addSubview(mapView)
-        
+        view.addSubview(addAddressButton)
+    }
+    
+    @objc func addAddressButtonTapped() {
+        print("addAddressButtonTapped")
     }
 
 }
@@ -37,7 +49,12 @@ extension ViewController {
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            
+            addAddressButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            addAddressButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            addAddressButton.widthAnchor.constraint(equalToConstant: 100),
+            addAddressButton.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
 }
