190c7d8402a5ff2a2cbd8a4984d9fd39340b43b6
Scooterminsk <kickscooterby@gmail.com>
2022-10-20 16:50:18 +0300

    MapView was added

--- RouteTestTask/Controllers/ViewController.swift
+++ RouteTestTask/Controllers/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setConstraints()
     }
 
+    private func setupViews() {
+        view.addSubview(mapView)
+        
+    }
 
 }
 
+extension ViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
+}
