3120f1039f9f39140a5bafbb85847e1afd11faaa
Scooterminsk <kickscooterby@gmail.com>
2022-10-20 17:34:38 +0300

    routeButton was created and added

--- RouteTestTask/Controllers/ViewController.swift
+++ RouteTestTask/Controllers/ViewController.swift
@@ -18,9 +18,17 @@ class ViewController: UIViewController {
     }()
     
     lazy var addAddressButton: UIButton = {
-        let button = UIButton()
+       let button = UIButton()
         button.setImage(UIImage(named: "AddAddress"), for: .normal)
         button.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var routeButton: UIButton = {
+       let button = UIButton()
+        button.setImage(UIImage(named: "Route"), for: .normal)
+        button.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -35,10 +43,15 @@ class ViewController: UIViewController {
     private func setupViews() {
         view.addSubview(mapView)
         view.addSubview(addAddressButton)
+        view.addSubview(routeButton)
     }
     
     @objc func addAddressButtonTapped() {
         print("addAddressButtonTapped")
+    }
+    
+    @objc func routeButtonTapped() {
+        print("routeButtonTapped")
     }
 
 }
@@ -51,10 +64,17 @@ extension ViewController {
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             
-            addAddressButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
-            addAddressButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            addAddressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAddressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
             addAddressButton.widthAnchor.constraint(equalToConstant: 100),
-            addAddressButton.heightAnchor.constraint(equalToConstant: 100)
+            addAddressButton.heightAnchor.constraint(equalToConstant: 100),
+            
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            routeButton.widthAnchor.constraint(equalToConstant: 100),
+            routeButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            
         ])
     }
 }
